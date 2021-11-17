6ded6e72c00a7ceea19c4528f93cad96f25c8dba
Masairo Obuchi <o.masahiro0328@gmail.com>
2021-11-18 00:34:11 +0900

    first commit

---
+++ hokudai-hitachi2021_A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <cassert>
+#define rep(i, n) for (int i = 0; i < (n); i++)
+using namespace std;
+using ll = long long;
+using P = pair<int, int>;
+/*
+struct Edge
+{
+  ll to;
+  ll weight;
+  Edge(ll t, ll w) : to(t), weight(w) {}
+};
+using Graph = vector<vector<Edge>>;
+*/
+using Graph = vector<vector<int>>;
+
+const long long INF = 1LL << 60;
+const int INT_INF = 1000000000;
+
+int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+// int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1}, dy[8] = {-1, 0, 1, 1, -1, 1, 0, -1};
+
+uint64_t get_rand_range(uint64_t min_val, uint64_t max_val)
+{
+    // 乱数生成器
+    static std::mt19937_64 mt64(0);
+
+    // [min_val, max_val] の一様分布整数 (int) の分布生成器
+    std::uniform_int_distribution<uint64_t> get_rand_uni_int(min_val, max_val);
+
+    // 乱数を生成
+    return get_rand_uni_int(mt64);
+}
+
+double get_rand_range_real(double min_val, double max_val)
+{
+    // 乱数生成器
+    static std::mt19937_64 mt64(0);
+
+    // [min_val, max_val] の一様分布整数 (int) の分布生成器
+    std::uniform_real_distribution<double> get_rand_real(min_val, max_val);
+
+    // 乱数を生成
+    return get_rand_real(mt64);
+}
+
+double get_rand_normal(double mu = 0.0, double sig = 1.0)
+{
+    static std::mt19937_64 mt64(0);
+
+    std::normal_distribution<double> get_rand(mu, sig);
+
+    return get_rand(mt64);
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    return 0;
+}
